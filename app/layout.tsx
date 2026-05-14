9a41a8d32ed85ba0463078b1fcfe475eea1747d1
notioneventagency-arch <notioneventagency@gmail.com>
2026-05-14 16:55:29 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,37 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import { ThemeProvider } from 'next-themes'
+
+const _geist = Geist({ subsets: ["latin"] });
+const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Notion Agency',
-  description: 'Creative Event & Media Agency',
+  title: 'PADELCLUBBING',
+  description: 'PADEL. MUSIC. COMMUNITY.',
+  generator: 'v0.app',
+
+  icons: {
+    icon: [
+      {
+        url: '/icon-light-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-        >
-          {children}
-        </ThemeProvider>
+    <html lang="en" className="bg-background">
+      <body className="font-sans antialiased">
+        {children}
+        {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
     </html>
   )
